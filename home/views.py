0db0f8108810f0a16f82c47b00e52510ee92eee2
Mahesh <pimpalemahesh2021@gmail.com>
2023-12-31 12:54:17 +0530

    ready to deployment

--- home/views.py
+++ home/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from products.models import Product
+from django.contrib.auth.decorators import login_required
 
-
-
+@login_required
 def index(request):
 
     context = {'products' : Product.objects.all()}
